e004691ac531bb4903a3570b607d674bbb56f2d1
b-mahjour <bmahjour@umich.edu>
2024-02-26 12:51:05 -0500

    maybe

---
+++ __init__.py
@@ -0,0 +1,6 @@
+from rdcanon.main import canon_smarts
+from rdcanon.main import canon_reaction_smarts
+from rdcanon.main import random_smarts
+from rdcanon.main import debug
+from rdcanon.main import gen_canon_repl_dict
+from rdcanon.token_parser import order_token_canon
